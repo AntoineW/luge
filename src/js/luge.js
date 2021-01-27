a26202da2603e3e58fc0b0a87a1f6cbb34d472b0
Antoine Wodniack <antoine@wodniack.fr>
2021-01-27 12:46:23 +0100

    expose public methods

--- src/js/luge.js
+++ src/js/luge.js
@@ -1,9 +1,11 @@
+// Imports
 import Actions from 'Luge/Actions'
 
 import Polyfill from 'Luge/Polyfill'
 
 import Luge from 'Luge/Core'
 
+import Emitter from 'Luge/Emitter'
 import LottiePlayer from 'Luge/LottiePlayer'
 import Parallax from 'Luge/Parallax'
 import Preloader from 'Luge/Preloader'
@@ -11,6 +13,14 @@ import Reveal from 'Luge/Reveal'
 import SmoothScroll from 'Luge/SmoothScroll'
 import Sticky from 'Luge/Sticky'
 import Transition from 'Luge/Transition'
+
+// Public methods
+window.luge = {
+  addAction: Actions.add.bind(Actions),
+  on: Emitter.on.bind(Emitter),
+  once: Emitter.once.bind(Emitter),
+  off: Emitter.off.bind(Emitter)
+}
 
 // Site init on DOM ready
 document.addEventListener('DOMContentLoaded', Actions.flow.bind(Actions, 'load'), { once: true })
