fd1b6184632b0003f0fd465fa3b4fc12722aa456
Antoine Wodniack <antoine@wodniack.fr>
2021-02-22 19:07:22 +0100

    scroll control lottie

--- src/js/luge/LottiePlayer.js
+++ src/js/luge/LottiePlayer.js
@@ -11,6 +11,7 @@ class LottiePlayer {
 
     // Listeners
     this.onViewportIntersect = this.onViewportIntersect.bind(this)
+    this.onScrollProgress = this.onScrollProgress.bind(this)
 
     if (document.readyState === 'complete') {
       this.addHooks()
@@ -68,6 +69,10 @@ class LottiePlayer {
       element.removeEventListener('revealin', self.play)
       element.removeEventListener('viewportintersect', self.onViewportIntersect)
 
+      if (element.hasAttribute('data-lg-lottie-scroll')) {
+        element.removeEventListener('scrollprogress', self.onScrollProgress)
+      }
+
       if (element.player) {
         element.player.destroy()
       }
@@ -116,47 +121,54 @@ class LottiePlayer {
     element.setAttribute('data-lg-lottie-state', 'is-paused')
 
     // Get options
+    var scroll = element.hasAttribute('data-lg-lottie-scroll')
     var loop = element.hasAttribute('data-lg-lottie-loop')
     var loopFrame = Number(element.getAttribute('data-lg-lottie-loop-frame'))
     var reverse = element.hasAttribute('data-lg-lottie-reverse')
 
-    element.player.addEventListener('enterFrame', function () {
-      if (element.player.totalFrames > 0) {
-        var currentFrame = Math.round(element.player.currentFrame)
+    if (scroll) {
+      ScrollObserver.add(element)
 
-        if (element.player.playDirection === 1) {
-          if (currentFrame === element.player.totalFrames - 1) {
-            element.player.pause()
+      element.addEventListener('scrollprogress', this.onScrollProgress)
+    } else {
+      element.player.addEventListener('enterFrame', function () {
+        if (element.player.totalFrames > 0) {
+          var currentFrame = Math.round(element.player.currentFrame)
 
-            if (reverse) {
-              setTimeout(() => {
-                element.player.setDirection(-1)
-                element.player.goToAndPlay(element.player.totalFrames, true)
+          if (element.player.playDirection === 1) {
+            if (currentFrame === element.player.totalFrames - 1) {
+              element.player.pause()
 
-                element.setAttribute('data-lg-lottie-state', 'is-playing is-playing--backward')
-              }, 0)
-            } else if (loop) {
-              setTimeout(() => {
-                element.player.goToAndPlay(loopFrame, true)
-              }, 0)
+              if (reverse) {
+                setTimeout(() => {
+                  element.player.setDirection(-1)
+                  element.player.goToAndPlay(element.player.totalFrames, true)
+
+                  element.setAttribute('data-lg-lottie-state', 'is-playing is-playing--backward')
+                }, 0)
+              } else if (loop) {
+                setTimeout(() => {
+                  element.player.goToAndPlay(loopFrame, true)
+                }, 0)
+              }
             }
-          }
-        } else {
-          if (currentFrame === loopFrame) {
-            element.player.pause()
+          } else {
+            if (currentFrame === loopFrame) {
+              element.player.pause()
 
-            if (loop) {
-              setTimeout(() => {
-                element.player.setDirection(1)
-                element.player.goToAndPlay(loopFrame, true)
+              if (loop) {
+                setTimeout(() => {
+                  element.player.setDirection(1)
+                  element.player.goToAndPlay(loopFrame, true)
 
-                element.setAttribute('data-lg-lottie-state', 'is-playing is-playing--forward')
-              }, 0)
+                  element.setAttribute('data-lg-lottie-state', 'is-playing is-playing--forward')
+                }, 0)
+              }
             }
           }
         }
-      }
-    })
+      })
+    }
 
     // Set methods
     element.play = this.play
@@ -191,6 +203,16 @@ class LottiePlayer {
     this.player.goToAndPlay(0, true)
 
     this.setAttribute('data-lg-lottie-state', 'is-playing is-playing--forward')
+  }
+
+  /**
+   * Scroll progress event handler
+   * @param {Event} e Custom event
+   */
+  onScrollProgress (e) {
+    var element = e.target
+
+    element.player.goToAndStop(element.player.totalFrames * element.scrollProgress, true)
   }
 }
 
