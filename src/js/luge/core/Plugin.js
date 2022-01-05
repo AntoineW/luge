6ee268fca974e658c4e89d84ed0c132985f95219
Antoine Wodniack <hello@wodniack.dev>
2022-01-05 10:59:22 +0100

    [Global] Combine plugin and feature classes

--- src/js/luge/core/Plugin.js
+++ src/js/luge/core/Plugin.js
@@ -1,12 +1,65 @@
 import Helpers from 'Core/Helpers'
-import Feature from 'Core/Feature'
+import LifeCycle from 'Core/LifeCycle'
+import Luge from 'Core/Core'
 
-export default class Plugin extends Feature {
+export default class Plugin {
+  /**
+   * Constructor
+   */
+  constructor (slug) {
+    this.pluginSlug = slug
+
+    LifeCycle.add('siteInit', this.beforeInit.bind(this), 5)
+  }
+
+  /**
+   * Before init
+   */
+  beforeInit (done) {
+    this.isDisabled = this.disabled()
+
+    if (this.isDisabled) {
+      document.documentElement.classList.add('lg-' + this.pluginSlug + '-disabled')
+    } else {
+      this.init()
+    }
+
+    done()
+  }
+
   /**
    * Init plugin
    */
   init () {
     this.setAttributes()
+  }
+
+  /**
+   * Check if feature is disabled
+   */
+  disabled () {
+    let disabled = false
+    if (typeof Luge.settings[this.pluginSlug] !== 'undefined' && typeof Luge.settings[this.pluginSlug].disabled !== 'undefined') {
+      disabled = Luge.settings[this.pluginSlug].disabled
+    }
+
+    if (Helpers.isString(disabled)) {
+      disabled = window.browser.is(disabled, true)
+    } else if (Helpers.isArray(disabled)) {
+      disabled = disabled.some(element => {
+        if (Helpers.isString(element)) {
+          return window.browser.is(element, true)
+        } else if (Helpers.isObject(element)) {
+          return window.browser.satisfies(element)
+        }
+
+        return false
+      })
+    } else if (Helpers.isObject(disabled)) {
+      disabled = window.browser.satisfies(disabled)
+    }
+
+    return disabled
   }
 
   /**
@@ -25,7 +78,7 @@ export default class Plugin extends Feature {
 
     for (const name in attributes) {
       const attribute = attributes[name]
-      let attributeKey = 'lg-' + this.featureSlug
+      let attributeKey = 'lg-' + this.pluginSlug
       if (name !== 'root') {
         attributeKey += '-' + name
       }
@@ -60,7 +113,7 @@ export default class Plugin extends Feature {
     }
 
     if (element.luge || (element.luge = {})) {
-      element.luge[this.featureSlug] = data
+      element.luge[this.pluginSlug] = data
     }
 
     return data
